8e916a09193be447cd2d48b67e77bfa7f9af5782
Diego García-Viana <diego.garcia-viana@thermofisher.com>
2019-09-04 16:16:18 +0200

    APM Angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component'
 
 @NgModule({
-  declarations: [ AppComponent ], // Which of our components belong to this module
-  imports: [ BrowserModule ], // External modules we want to have available to all of the components that belong to this module
-  bootstrap: [ AppComponent ] // Startup component of the application
+  // Which of our components belong to this module
+  declarations: [ AppComponent, ProductListComponent ], 
+  /* External modules we want to have available to all of the components that 
+  belong to this module*/
+  imports: [ BrowserModule, FormsModule ], 
+  // Startup component of the application
+  bootstrap: [ AppComponent ] 
 })
 
 export class AppModule { }
