cedd723e7bf5113beed3cf909d46aefc8a35324a
Diego García-Viana <diego.garcia-viana@thermofisher.com>
2019-09-09 07:38:58 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 import { ConvertToSpacesPipe } from './shared/pipe/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star/star.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   // Which of our components belong to this module
@@ -14,14 +18,24 @@ import { StarComponent } from './shared/star/star.component';
     AppComponent, 
     ProductListComponent, 
     ConvertToSpacesPipe, 
-    StarComponent 
+    StarComponent, 
+    ProductDetailsComponent,
+    WelcomeComponent,
+    PageNotFoundComponent 
   ], 
   /* External modules we want to have available to all of the components that 
   belong to this module*/
   imports: [ 
     BrowserModule, 
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailsComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', component: PageNotFoundComponent }
+    ])
   ], 
   // Startup component of the application
   bootstrap: [ AppComponent ],
---
+++ src/app/products/product-details/product-details.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+  pageTitle: string = 'Hey';
+}
