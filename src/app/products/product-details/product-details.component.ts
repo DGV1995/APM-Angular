8ce1d89de1cf6485728bff91966ff80a8add83ef
Diego García-Viana <diego.garcia-viana@thermofisher.com>
2019-09-09 08:37:42 +0200

    Routing

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,10 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/shared/service/product.service';
 
 @Component({
   selector: 'pm-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
-  pageTitle: string = 'Hey';
+export class ProductDetailsComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    console.log(this.route.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    // '+' symbol turns the parameter id into an integer
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
